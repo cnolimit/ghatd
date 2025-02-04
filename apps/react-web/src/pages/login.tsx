521b4486a133b4c5b8f160db47a086c503adaed2
Phillip Boateng <cnolimit@gmail.com>
2025-02-04 23:26:34 +0000

    add tailwind config for v4

--- apps/react-web/src/pages/login.tsx
+++ apps/react-web/src/pages/login.tsx
@@ -1,12 +1,34 @@
-import Layout from '../layout'
-
-function Login() {
-
+const Login = () => {
   return (
-    <Layout>
-      <h1> This is the Login page</h1>
-    </Layout>
-  )
-}
+    <div className="col-span-full flex flex-col items-center justify-center">
+      <form
+        className="mb-4 w-full max-w-sm rounded-lg bg-white p-4 shadow-md"
+        method="post"
+      >
+        <div className="flex flex-col gap-4">
+          <label
+            htmlFor="email"
+            className="flex flex-col text-sm font-semibold text-gray-700"
+          >
+            Email
+            <input
+              className="mt-1 rounded-md border border-gray-300 p-2 focus:border-transparent focus:ring-2 focus:ring-blue-500 focus:outline-none"
+              placeholder="email@site.com"
+              id="email"
+              type="email"
+              required
+            />
+          </label>
+          <button
+            type="submit"
+            className="mt-4 w-full rounded-md bg-blue-600 px-4 py-2 text-white hover:bg-blue-700 focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:outline-none"
+          >
+            Login
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
--- apps/react-web/src/router.tsx
+++ apps/react-web/src/router.tsx
@@ -1,21 +1,20 @@
-import { Route, Routes } from "react-router";
-import { AuthLayout } from "./layouts/auth-layout";
+import { Route, Routes } from 'react-router';
+import { AuthLayout } from './layouts/auth-layout';
 import About from './pages/about';
 import Home from './pages/home';
-import Login from "./pages/login";
+import Login from './pages/login';
 
 export default () => (
   <Routes>
     <Route index element={<Home />} />
     <Route path="about" element={<About />} />
 
-  <Route element={<AuthLayout />}>
-    <Route path="login" element={<Login />} />
-    {/* <Route path="register" element={<Register />} /> */}
-  </Route>
+    <Route element={<AuthLayout />}>
+      <Route path="login" element={<Login />} />
+      {/* <Route path="register" element={<Register />} /> */}
+    </Route>
 
-  {/* <Route path=
+    {/* <Route path=
   \ */}
   </Routes>
-
-)
+);
