cf7ca1c5d26d43c4cb6bbbbefdd060b9cf12fb82
Phillip Boateng <cnolimit@gmail.com>
2025-02-05 12:51:13 +0000

    upgraded to tailwind v4

--- apps/react-web/src/pages/login.tsx
+++ apps/react-web/src/pages/login.tsx
@@ -12,7 +12,7 @@ const Login = () => {
           >
             Email
             <input
-              className="mt-1 rounded-md border border-gray-300 p-2 focus:border-transparent focus:ring-2 focus:ring-blue-500 focus:outline-none"
+              className="mt-1 rounded-md border border-gray-300 p-2 focus:border-transparent focus:ring-2 focus:ring-blue-500 focus:outline-hidden"
               placeholder="email@site.com"
               id="email"
               type="email"
@@ -21,7 +21,7 @@ const Login = () => {
           </label>
           <button
             type="submit"
-            className="mt-4 w-full rounded-md bg-blue-600 px-4 py-2 text-white hover:bg-blue-700 focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:outline-none"
+            className="mt-4 w-full rounded-md bg-blue-600 px-4 py-2 text-white hover:bg-blue-700 focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:outline-hidden"
           >
             Login
           </button>
